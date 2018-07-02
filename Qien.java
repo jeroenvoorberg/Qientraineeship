ff3972bb193ea576a29182948ad78912575f5a10
Dudezer0 <123leefelkedag>
2018-07-02 11:49:03 +0200

    tweede commit

--- Qien.java
+++ Qien.java
@@ -9,11 +9,12 @@ class Qien{
 		if(leeftijd < geenAlcohol){
 			System.out.println("Je mag helaas geen alcohol, je bent jonger dan 16");		
 		}else{
-			if(leeftijd >= geenAlcohol & leeftijd <= alleenBierOfWijn){
+			if(leeftijd >= geenAlcohol && leeftijd <= alleenBierOfWijn){
 				System.out.println("Je mag alleen bier of wijn, je bent tussen de 16 en 18");
 			}else{
 				System.out.println("Je mag drinken wat je wil, proost");
-			}		
+			}
+			System.out.println("extra lijn");		
 		}
 	}
 }
